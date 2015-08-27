417c6ed005589da4b1144a32de9a3912fcb0142f
Burhan <bern.3759@yahoo.com>
2015-08-28 00:17:29 +0800

    Edited pillReminder

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','starter.login', 'starter.pillBoxMode','starter.pairing'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','starter.login', 'starter.pillBoxMode','starter.pairing','starter.mainCtrl'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
