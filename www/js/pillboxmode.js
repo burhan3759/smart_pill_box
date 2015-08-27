c087dbd84fa3f7b4012fd2e8817935fee9b20909
Joel Yek <joevo2@gmail.com>
2015-08-27 19:58:23 +0800

    pairing code will send code to parse for pairing

--- www/js/pillboxmode.js
+++ www/js/pillboxmode.js
@@ -5,6 +5,38 @@ angular.module('starter.pillBoxMode', [])
 })
 
 .controller('PairCtrl', function($scope) {
-  var a = (Math.floor(1000 + Math.random() * 9000)).toString();
-  $scope.code = a.substring(-2);
+  if (localStorage[':DID'] != undefined) {
+    //Get from localStorage
+    console.log("not empty");
+    $scope.code = JSON.parse(localStorage[':DID'] || '{}');
+  } else {
+    console.log("empty");
+    //Generate random number
+    var a = (Math.floor(1000 + Math.random() * 9000)).toString();
+    $scope.code = a.substring(-2);
+    //Store in localStorage
+    localStorage[':DID'] = JSON.stringify($scope.code);
+
+    var Pillbox = Parse.Object.extend("Pillbox");
+    var pillbox = new Pillbox();
+    
+    pillbox.set("Pid", $scope.code);
+
+    pillbox.save(null, {
+      success: function(pillbox) {
+        // Execute any logic that should take place after the object is saved.
+        alert('New object created with objectId: ' + pillbox.id);
+      },
+      error: function(pillbox, error) {
+        // Execute any logic that should take place if the save fails.
+        // error is a Parse.Error with an error code and message.
+        alert('Failed to create new object, with error code: ' + error.message);
+      }
+    });
+  }
+
+  $scope.clear = function() {
+    window.localStorage.clear();
+    window.location.reload(true);
+  };
 });
