643f19a3a1d40200fe4c81208b647a0389075151
Joel Yek <joevo2@gmail.com>
2015-08-27 18:30:17 +0800

    fixed "separate pillbox mode.js"

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -29,14 +29,7 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('PillboxCtrl', function($scope) {
 
-})
-
-.controller('PairCtrl', function($scope) {
-  var a = (Math.floor(1000 + Math.random() * 9000)).toString();
-  $scope.code = a.substring(-2);
-}) 
 
 .controller ('ReminderCtrl', function () {
 
