9f3f1f87867e98ff35eb5c5d7b1ca569085a71e9
Joel Yek <joevo2@gmail.com>
2015-08-27 20:26:08 +0800

    started prototyping with accelerometer

--- www/js/pillboxmode.js
+++ www/js/pillboxmode.js
@@ -1,7 +1,50 @@
 angular.module('starter.pillBoxMode', [])
 
-.controller('PillboxCtrl', function($scope) {
-
+.controller('PillboxCtrl', function($scope, $cordovaDeviceMotion) {
+    //Motion sensing stuff
+    // document.addEventListener("deviceready", function () {
+  
+    //   $cordovaDeviceMotion.getCurrentAcceleration().then(function(result) {
+    //     var X = result.x;
+    //     var Y = result.y;
+    //     var Z = result.z;
+    //     var timeStamp = result.timestamp;
+    //   }, function(err) {
+    //     // An error occurred. Show a message to the user
+    //   });
+  
+    // }, false);
+  
+  
+    // watch Acceleration
+    var options = { frequency: 20000 };
+  
+    document.addEventListener("deviceready", function () {
+  
+      var watch = $cordovaDeviceMotion.watchAcceleration(options);
+      watch.then(
+        null,
+        function(error) {
+        // An error occurred
+        },
+        function(result) {
+          $scope.accel.X = result.x;
+          $scope.accel.Y = result.y;
+          $scope.accel.Z = result.z;
+          $scope.accel.timeStamp = result.timestamp;
+      });
+  
+  
+      // watch.clearWatch();
+      // // OR
+      // $cordovaDeviceMotion.clearWatch(watch)
+      //   .then(function(result) {
+      //     // success
+      //     }, function (error) {
+      //     // error
+      //   });
+  
+    }, false);
 })
 
 .controller('PairCtrl', function($scope) {
